98076a2ca3cd6c2511623dc1a59b41fbc3d86e18
Chris Yate <chrisyate@gmail.com>
2012-03-07 16:13:59 +0000

    added calcualte

--- calculate.py
+++ calculate.py
@@ -2,7 +2,9 @@ import os
 import urllib
 import urllib2
 import cookielib
-from itertools import groupby
+
+import numpy
+import scipy.linalg
 
 from login import LoggerInner
 from webUtils import WebUtils
@@ -24,7 +26,49 @@ class Calculator(object):
 
 	def Parse(self, inputLine):
 		equation = inputLine[inputLine.find('\"') + 1: inputLine.rfind('\"')]
-		print equation
+
+		equation = equation.replace("-", "+-")
+		equation = equation.replace("=", "=+")
+		equation = equation.replace("++", "+")		
+		parts = equation.split('=')
+
+		lhs = parts[0]
+		if lhs[0] == '+':
+			lhs = lhs[1:]
+		lhsparts = lhs.split('+')
+		
+		rhs = parts[1]		
+		if rhs[0] == '+':
+			rhs = rhs[1:]
+		rhsparts = rhs.split('+')
+
+		allparts = lhsparts
+	
+		for part in rhsparts:
+			if part[0] == '-':
+				allparts.append(part[1:])
+			else:
+				allparts.append("-" + part)
+
+		allparts_split = []
+		for p in allparts:
+			tp = p.split('*')
+			if len(tp) == 1:
+				tp = [tp[0], '1']
+			allparts_split.append(tp)
+
+
+		self.matrixSize = len(allparts_split)
+		self.unknownsValues = [t[1] for t in allparts_split]
+		self.coefficients = [t[0] for t in allparts_split]
+		
+		print self.matrixSize
+		print self.unknownsValues
+		print self.coefficients
+
+	def Solve(self):
+		
+
 
 LoggerInner(bs_username, bs_password, cookiefile)
 pageGetter = WebUtils(cookiefile, baseurl)
@@ -32,6 +76,10 @@ problem = pageGetter.getHTML(linkPath)
 print problem
 
 calculator = Calculator(problem)
+
+calculator.Solve()
+
+print calculator.result
 
 solutionParams = "?solution="
 solpath = "%s%s%s" %(solutionPath, solutionParams, "")
